6c9b9ce250e7d75681e3c52ac3bdfa4cfaf2eaa1
Evgenii <johncom1000@mail.ru>
2022-01-21 14:53:01 +0600

    init fix (2)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import app from './app.js';
 
 app();
+
+export default app;
